502877238cde332f71304ea6f1f41cc692c90185
Nick Kocharhook <nick@kocharhook.com>
2020-02-25 22:33:13 +0000

    Pretty much done now. Added random and key options.

--- encode_to_binary.py
+++ encode_to_binary.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import argparse
+import random
 
 # https://youtu.be/USCBCmwMCDA
 
@@ -9,28 +10,31 @@ def parseArguments():
 	parser = argparse.ArgumentParser(description='Create skeleton CSVs')
 
 	parser.add_argument('--random', type=int, default=0, help='How many characters of randomness')
+	parser.add_argument('--key', action='store_true', help='Print character to number to binary key')
 
 	return parser.parse_args()
 
 
 def main():
 	args = parseArguments()
-	chars = '.-/049:=?ABCDQWXabcdeghmopstuvwy'
+	chars = '.-/&49:=?ABCDQWXabcdeghmopstuvwy'
+	string = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&'
+	binary = []
 
 	if args.random > 0:
-		# do random
-		pass
+		string = ''.join(random.choices(list(chars), k=args.random))
 
-	else:
-		string = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-		binary = []
+	for char in string:
+		# print(char)
+		index = chars.index(char)
+		binary.append(f'{index:05b}')
 
-		for char in string:
-			# print(char)
+	if args.key:
+		for char in chars:
 			index = chars.index(char)
-			binary.append(f'{index:05b}')
-
-		print(" ".join(binary))
+			print(f'{char}\t{index}\t{index:05b}')
+	else:
+		print(" ".join(list(binary)))
 
 
 # Default function is main()
